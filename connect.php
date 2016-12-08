e80b883c8b7e080f0ac4c31c301d46cb7b8a6c6d
Damian-Roszak <zakladylaboratoryjneroszaka@gmail.com>
2016-12-08 06:17:36 +0100

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,14 +1,8 @@
 <?php
 
 	$host = "localhost";
-	$db_user = "root";
-	$db_password = "asd";
+	$db_user = "pup";
+	$db_password = "ciuciu";
 	$db_name = "szubienica";
 
-	
-/*	$host = "mysql1.ugu.pl";
-	$db_user = "db687831";
-	$db_password = "bW9h9JewycTvfmnq";
-	$db_name = "db687831";
-*/
 ?>
